a7e7bcbd1d8e6e2e9d37bd29cde2bd1cd498817b
RTCWDF\chinnam <harish.chinnam@realtech.com>
2022-10-30 21:44:57 +0100

    Accessing the deployed contract on Goerli network

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,9 +1,10 @@
+// factory.js
 import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0xF2869cde6455378Eb3462C0451849d8b254d6841'
+    '0x276d02e677F7062294f52daDae42CefBDFD1CdfA'
 );
 
 export default instance;
